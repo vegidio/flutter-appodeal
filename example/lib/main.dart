11570c049542fbb745d2a16fab3cf8375dac7d57
devuserPP <devuser@example.com>
2021-09-09 15:48:44 +0200

    updated example with null safety

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:appodeal_flutter/appodeal_flutter.dart';
 import 'package:flutter/material.dart';
-import 'package:toast/toast.dart';
+import 'package:fl_toast/fl_toast.dart';
 
 void main() {
   runApp(MyApp());
@@ -20,15 +20,20 @@ class _MyAppState extends State<MyApp> {
 
     // Set the app keys
     Appodeal.setAppKeys(
-        androidAppKey: 'f1e6435dfa48cb71bb6753f1c8ac97bba6609d481e63bb98',
-        iosAppKey: '3a2ef99639e29dfe3333e4b3b496964dae6097cc510cbb2f');
+      androidAppKey: 'f1e6435dfa48cb71bb6753f1c8ac97bba6609d481e63bb98',
+    );
 
     // Defining the callbacks
-    Appodeal.setBannerCallback((event) => print('Banner ad triggered the event $event'));
-    Appodeal.setMrecCallback((event) => print('MREC ad triggered the event $event'));
-    Appodeal.setInterstitialCallback((event) => print('Interstitial ad triggered the event $event'));
-    Appodeal.setRewardCallback((event) => print('Reward ad triggered the event $event'));
-    Appodeal.setNonSkippableCallback((event) => print('Non-skippable ad triggered the event $event'));
+    Appodeal.setBannerCallback(
+        (event) => print('Banner ad triggered the event $event'));
+    Appodeal.setMrecCallback(
+        (event) => print('MREC ad triggered the event $event'));
+    Appodeal.setInterstitialCallback(
+        (event) => print('Interstitial ad triggered the event $event'));
+    Appodeal.setRewardCallback(
+        (event) => print('Reward ad triggered the event $event'));
+    Appodeal.setNonSkippableCallback(
+        (event) => print('Non-skippable ad triggered the event $event'));
 
     // Request authorization to track the user
     Appodeal.requestIOSTrackingAuthorization().then((_) async {
@@ -38,7 +43,13 @@ class _MyAppState extends State<MyApp> {
       // Initialize Appodeal after the authorization was granted or not
       await Appodeal.initialize(
           hasConsent: true,
-          adTypes: [AdType.BANNER, AdType.MREC, AdType.INTERSTITIAL, AdType.REWARD, AdType.NON_SKIPPABLE],
+          adTypes: [
+            AdType.BANNER,
+            AdType.MREC,
+            AdType.INTERSTITIAL,
+            AdType.REWARD,
+            AdType.NON_SKIPPABLE
+          ],
           testMode: true);
 
       setState(() => this.isAppodealInitialized = true);
@@ -72,8 +83,12 @@ class _Body extends StatelessWidget {
                 child: Text('Should I collect user consent?'),
                 onPressed: () async {
                   var shouldShow = await Appodeal.shouldShowConsent();
-                  Toast.show('The app should${shouldShow ? ' ' : ' NOT '}collect user consent', context,
-                      duration: Toast.LENGTH_LONG, gravity: Toast.BOTTOM);
+
+                  await showPlatformToast(
+                      child: Text(
+                          'The app should${shouldShow ? ' ' : ' NOT '}collect user consent'),
+                      context: context,
+                      duration: Duration(seconds: 4));
                 },
               ),
               ElevatedButton(
@@ -81,8 +96,12 @@ class _Body extends StatelessWidget {
                 onPressed: () async {
                   var consent = await Appodeal.fetchConsentInfo();
 
-                  Toast.show('Status: ${consent.status}; Zone: ${consent.zone}', context,
-                      duration: Toast.LENGTH_LONG, gravity: Toast.BOTTOM);
+                  showPlatformToast(
+                    child: Text(
+                        'Status: ${consent.status}; Zone: ${consent.zone}'),
+                    context: context,
+                    duration: Duration(seconds: 4),
+                  );
                 },
               ),
               ElevatedButton(
@@ -94,9 +113,15 @@ class _Body extends StatelessWidget {
               ElevatedButton(
                 child: Text('Is Interstitial Ad ready for show?'),
                 onPressed: () async {
-                  var isReady = await Appodeal.isReadyForShow(AdType.INTERSTITIAL);
-                  Toast.show(isReady ? 'Interstitial ad is ready' : 'Interstitial ad is NOT ready', context,
-                      duration: Toast.LENGTH_LONG, gravity: Toast.BOTTOM);
+                  var isReady =
+                      await Appodeal.isReadyForShow(AdType.INTERSTITIAL);
+                  showPlatformToast(
+                    child: Text(isReady
+                        ? 'Interstitial ad is ready'
+                        : 'Interstitial ad is NOT ready'),
+                    context: context,
+                    duration: Duration(seconds: 4),
+                  );
                 },
               ),
               ElevatedButton(
@@ -108,38 +133,57 @@ class _Body extends StatelessWidget {
               ElevatedButton(
                 child: Text('Show Interstitial Ad'),
                 onPressed: () async {
-                  await Appodeal.show(AdType.INTERSTITIAL, placementName: "placement-name");
+                  await Appodeal.show(AdType.INTERSTITIAL,
+                      placementName: "placement-name");
                 },
               ),
               ElevatedButton(
                 child: Text('Is Reward Ad ready for show?'),
                 onPressed: () async {
                   var isReady = await Appodeal.isReadyForShow(AdType.REWARD);
-                  Toast.show(isReady ? 'Reward ad is ready' : 'Reward ad is NOT ready', context,
-                      duration: Toast.LENGTH_LONG, gravity: Toast.BOTTOM);
+                  showPlatformToast(
+                    child: Text(isReady
+                        ? 'Reward ad is ready'
+                        : 'Reward ad is NOT ready'),
+                    context: context,
+                    duration: Duration(seconds: 4),
+                  );
                 },
               ),
               ElevatedButton(
                 child: Text('Show Reward Ad'),
                 onPressed: () async {
-                  var status = await Appodeal.show(AdType.REWARD, placementName: 'placement-name');
+                  var status = await Appodeal.show(AdType.REWARD,
+                      placementName: 'placement-name');
                   print(status);
                 },
               ),
               ElevatedButton(
                 child: Text('Is Non-Skippable Ad ready?'),
                 onPressed: () async {
-                  var isReady = await Appodeal.isReadyForShow(AdType.NON_SKIPPABLE);
-                  Toast.show(isReady ? 'Non-Skippable ad is ready' : 'No-Skippable ad is NOT ready', context,
-                      duration: Toast.LENGTH_LONG, gravity: Toast.BOTTOM);
+                  var isReady =
+                      await Appodeal.isReadyForShow(AdType.NON_SKIPPABLE);
+                  showPlatformToast(
+                    child: Text(isReady
+                        ? 'Non-Skippable ad is ready'
+                        : 'Non-Skippable ad is NOT ready'),
+                    context: context,
+                    duration: Duration(seconds: 4),
+                  );
                 },
               ),
               ElevatedButton(
                 child: Text('Can Show Non-Skippable Ad?'),
                 onPressed: () async {
-                  var canShow = await Appodeal.canShow(AdType.NON_SKIPPABLE, placementName: "placement-name");
-                  Toast.show(canShow ? 'Non-Skippable can be shown' : 'Non-Skippable can NOT be shown', context,
-                      duration: Toast.LENGTH_LONG, gravity: Toast.BOTTOM);
+                  var canShow = await Appodeal.canShow(AdType.NON_SKIPPABLE,
+                      placementName: "placement-name");
+                  showPlatformToast(
+                    child: Text(canShow
+                        ? 'Non-Skippable can be shown'
+                        : 'Non-Skippable can NOT be shown'),
+                    context: context,
+                    duration: Duration(seconds: 4),
+                  );
                 },
               ),
               ElevatedButton(
